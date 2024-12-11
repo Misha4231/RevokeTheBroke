b0ea5c01ce853a6935bbc51a4a839c9d9f7ed6e3
Misha4231 <mishagalko1@gmail.com>
2024-12-11 08:30:24 +0000

    helper function for cookies

--- revoke_the_broke/categories/views.py
+++ revoke_the_broke/categories/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+import datetime
+from django.conf import settings
+from .models import Category
 
-# Create your views here.
 def index(request):
+    
+
     return render(request, 'categories/index.html')
+
+
+# funkcja pomocnicza dla ustalenia cookie (głównie jest wykorzystywana dla trybu incognito)
+def set_cookie(response, key, value, days_expire=7):
+    if days_expire is None:
+        max_age = 365 * 24 * 60 * 60  # one year
+    else:
+        max_age = days_expire * 24 * 60 * 60
+    expires = datetime.datetime.strftime(
+        datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age),
+        "%a, %d-%b-%Y %H:%M:%S GMT",
+    )
+    response.set_cookie(
+        key,
+        value,
+        max_age=max_age,
+        expires=expires,
+        domain=settings.SESSION_COOKIE_DOMAIN,
+        secure=settings.SESSION_COOKIE_SECURE or None,
+    )
