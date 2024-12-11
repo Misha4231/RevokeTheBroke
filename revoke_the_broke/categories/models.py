679dec75325c2783ba0b2f4bb928e3b305c9a38d
Misha4231 <mishagalko1@gmail.com>
2024-12-11 08:15:45 +0000

    add models

--- revoke_the_broke/categories/models.py
+++ revoke_the_broke/categories/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    incognito_user_token = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    color = models.CharField(max_length=7, default='#FFFFFF')
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name_plural = "Categories"
+
+        def __unicode__(self):
+            return self.title
--- revoke_the_broke/spendings/models.py
+++ revoke_the_broke/spendings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from categories.models import Category
 
-# Create your models here.
+
+class Expenditure(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    incognito_user_token = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created_at']
+
+        def __unicode__(self):
+            return self.title
