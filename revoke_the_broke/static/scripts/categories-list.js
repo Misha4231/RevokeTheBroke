d3900b8f451cf976372ebd9c511d6bcdb7d97dda
Misha4231 <mishagalko1@gmail.com>
2024-12-18 08:34:01 +0000

    fix list on index

--- revoke_the_broke/static/scripts/categories-list.js
+++ revoke_the_broke/static/scripts/categories-list.js
@@ -6,8 +6,21 @@ document.querySelectorAll('.category-colorbox').forEach(function(box) {
 });
 
 document.querySelectorAll('.expand-list-icon').forEach((icon) => {
-    icon.addEventListener('onclick', () => {
-        icon.parentElement.parentElement.querySelector('.category-desctiption-container').style.display = 'none'
-    })
-
-})
+    var parentDescription = icon.parentElement.parentElement.querySelector('.category-desctiption-container');
+    parentDescription.style.display = 'none'
+    icon.addEventListener('click', () => {
+        
+        
+        if (parentDescription.style.display === 'none') {
+            parentDescription.style.display = 'block';
+            
+            var openIconSrc = icon.querySelector('img').dataset.open;
+            icon.querySelector('img').setAttribute('src', openIconSrc);
+        } else {
+            parentDescription.style.display = 'none';
+            var closeIconSrc = icon.querySelector('img').dataset.close;
+            icon.querySelector('img').setAttribute('src', closeIconSrc);
+           
+        }
+    });
+});
